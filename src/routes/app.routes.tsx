251ea763790b61257b44792cb0b9d60c281ff969
Jones-bass <jonesbass.tb@gmail.com>
2022-06-06 23:12:05 -0300

    listando categorias

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -4,6 +4,7 @@ import {MaterialIcons} from '@expo/vector-icons'
 import { useTheme } from "styled-components";
 import { Register } from "../screen/Register";
 import Dashboard from "../screen/Dashboard";
+import { Resume } from "../screen/Resume";
 
 const {Navigator, Screen} = createBottomTabNavigator()
 
@@ -51,7 +52,7 @@ export function AppRoutes(){
             />
             <Screen
                 name="Resumo"
-                component={Register}
+                component={Resume}
                 options={{
                     tabBarIcon: (({size, color}) => 
                         <MaterialIcons
